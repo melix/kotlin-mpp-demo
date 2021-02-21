180c47b88fed90876a335b1ab29042829acf25e4
Cedric Champeau <cedric.champeau@gmail.com>
2021-02-21 22:12:42 +0100

    For fun (pun intended), a bit more code

--- src/commonMain/kotlin/hello.kt
+++ src/commonMain/kotlin/hello.kt
@@ -1,3 +1,13 @@
 fun main() {
-    println("Salut, Frédéric !")
+    println("Salut, Frédéric. Fib(90) = ${fib(90)}")
+}
+
+fun fib(n: Long): Long = fib(n, mutableMapOf())
+
+fun fib(n: Long, cache: MutableMap<Long, Long>): Long = cache.getOrPut(n) {
+    when (n) {
+        0L -> 0L
+        1L -> 1L
+        else -> fib(n - 1, cache) + fib(n - 2, cache)
+    }
 }
