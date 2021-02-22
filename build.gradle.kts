2a81ab568d8e30075107b1b542ec14314597a700
fcamblor <fcamblor@gmail.com>
2021-02-22 12:37:31 +0100

    Modified build.gradle sourcesets in order to have sources locates into the native sourceset to be able to access multiplatform libs like posix. This is something required if we want to have access to POSIX file manipulation functions, as we can't use java.(n)io.File class in multiplatform context

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,20 +11,35 @@ dependencies {
 }
 
 kotlin {
-    macosX64("native-mac") {
+    val hostOs = System.getProperty("os.name")
+    val isMingwX64 = hostOs.startsWith("Windows")
+    val nativeTarget = when {
+        hostOs == "Mac OS X" -> macosX64("native")
+        hostOs == "Linux" -> linuxX64("native")
+        isMingwX64 -> mingwX64("native")
+        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+    }
+
+    nativeTarget.apply {
         binaries {
             executable()
         }
     }
-    linuxX64("native-linux") {
-        binaries {
-            executable()
-        }
-    }
-    mingwX64("native-win") {
-        binaries {
-            executable()
-        }
-    }
+
+//    macosX64("native-mac") {
+//        binaries {
+//            executable()
+//        }
+//    }
+//    linuxX64("native-linux") {
+//        binaries {
+//            executable()
+//        }
+//    }
+//    mingwX64("native-win") {
+//        binaries {
+//            executable()
+//        }
+//    }
 }
 
